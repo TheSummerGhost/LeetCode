66240e7f7d929f846f6d709d5249336d229c217b
kiranosauras <kiranbharadia@gmail.com>
2024-02-06 23:39:14 +0000

    adding RemoveDuplicatesFromSortedArraySolution

---
+++ Easy/RemoveDuplicatesFromSortedArray.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+
+
+        pointer = 1
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[pointer] = nums[i]
+                pointer += 1
+        
+        return pointer
